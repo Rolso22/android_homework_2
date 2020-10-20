eafddf47d96c21ce0abc3458ebbc94a2abfc9d05
Rolso22 <ramagf@yandex.ru>
2020-10-20 22:49:25 +0300

    end

--- app/src/main/java/ru/mail/myapplication/NumberFragment.java
+++ app/src/main/java/ru/mail/myapplication/NumberFragment.java
@@ -12,13 +12,11 @@ import androidx.fragment.app.Fragment;
 
 public class NumberFragment extends Fragment {
 
-    private TextView num;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.big_number, container, false);
-        num = view.findViewById(R.id.big_number);
+        TextView num = view.findViewById(R.id.big_number);
         Bundle bundle = getArguments();
         num.setText(String.valueOf(bundle.getInt(MainActivity.POS)));
         num.setTextColor(bundle.getInt(MainActivity.COLOR));
