9c03df721a2053c0e1585aa2ef78ae1951875219
Rolso22 <ramagf@yandex.ru>
2020-10-22 14:08:17 +0300

    endless2

--- app/src/main/java/ru/mail/myapplication/ClickedClass.java
+++ app/src/main/java/ru/mail/myapplication/ClickedClass.java
@@ -6,28 +6,29 @@ import android.view.View;
 
 import androidx.fragment.app.FragmentActivity;
 
-public class ClickedClass {
+public class ClickedClass implements View.OnClickListener {
 
     private FragmentActivity act;
+    private final int n;
+    private final int color;
 
-    public ClickedClass(Activity act) {
+    public ClickedClass(Activity act, final int n, final int color) {
         this.act = (FragmentActivity) act;
+        this.n = n;
+        this.color = color;
     }
 
-    public View.OnClickListener onNumberClicked(final int n, final int color) {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putInt(MainActivity.POS, n);
-                bundle.putInt(MainActivity.COLOR, color);
-                NumberFragment nf = new NumberFragment();
-                nf.setArguments(bundle);
-                act.getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, nf)
-                        .addToBackStack(null)
-                        .commitAllowingStateLoss();
-            }
-        };
+    @Override
+    public void onClick(View v) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(MainActivity.POS, n);
+        bundle.putInt(MainActivity.COLOR, color);
+        NumberFragment nf = new NumberFragment();
+        nf.setArguments(bundle);
+        act.getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, nf)
+                .addToBackStack(null)
+                .commitAllowingStateLoss();
     }
 }
+
